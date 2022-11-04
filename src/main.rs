5b0cd317823f9a4819660ef1fdd640ee2ab43920
Michael de Silva <michael@mwdesilva.com>
2022-11-04 09:32:46 +0530

    Initial Wifi and I2C test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use core::time::Duration;
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use std::time::Duration;
 
 use embedded_hal::digital::v2::ToggleableOutputPin;
-use esp_idf_hal::{
-    peripherals::Peripherals,
-    units::FromValueType,
-    i2c::{self, MasterPins}
+use esp_idf_hal::gpio::{Gpio4, Gpio5, Unknown};
+use esp_idf_hal::i2c;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::units::FromValueType;
+use esp_idf_svc::netif::EspNetifStack;
+use esp_idf_svc::nvs::EspDefaultNvs;
+use esp_idf_svc::sysloop::EspSysLoopStack;
+use esp_idf_svc::wifi::EspWifi;
+
+use std::sync::Arc;
+
+use embedded_svc::wifi::{
+    ClientConfiguration, ClientConnectionStatus, ClientIpStatus, ClientStatus, Configuration, Wifi,
 };
+
+use anyhow::Result;
 
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
@@ -14,52 +25,112 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
 };
-use ssd1306::{
-    prelude::*,
-    I2CDisplayInterface,
-    Ssd1306,
-};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
-fn main() {
-    esp_idf_sys::link_patches();
-
-    //  configure peripherals
-    let peripherals = Peripherals::take().unwrap();
-    let mut led = peripherals.pins.gpio17.into_output().unwrap();
-    let mut led_onboard = peripherals.pins.gpio18.into_output().unwrap();
-    let sda = peripherals.pins.gpio4.into_input_output().unwrap();
-    let scl = peripherals.pins.gpio5.into_output().unwrap();
-
-    //  configure i2c
-    let _cfg = i2c::config::MasterConfig::new()
-        .baudrate(400.kHz().into());
+fn display_test(i2c0: i2c::I2C0, scl: Gpio4<Unknown>, sda: Gpio5<Unknown>, ip: &str) -> Result<()> {
     let i2c = i2c::Master::new(
-        peripherals.i2c0,
-        MasterPins { sda, scl },
-        i2c::config::MasterConfig::new()
-        .baudrate(400.kHz().into())
-    ).unwrap();
+        i2c0,
+        i2c::MasterPins {
+            scl: scl.into_output().unwrap(),       // O
+            sda: sda.into_input_output().unwrap(), // I+O
+        },
+        i2c::config::MasterConfig::new().baudrate(400.kHz().into()),
+    )?;
 
-    //  configure OLED display
     let interface = I2CDisplayInterface::new(i2c);
+
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
-    display.init().unwrap();
+    display
+        .init()
+        .map_err(|e| anyhow::anyhow!("Init error: {:?}", e))?;
 
     let text_style = MonoTextStyleBuilder::new()
         .font(&FONT_6X10)
         .text_color(BinaryColor::On)
         .build();
 
-    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+    Text::with_baseline("Hello Rust!", Point::zero(), text_style, Baseline::Top)
         .draw(&mut display)
-        .unwrap();
+        .map_err(|e| anyhow::anyhow!("Txt error: {:?}", e))?;
 
-    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
+    Text::with_baseline(
+        &format!("IP: {}", ip),
+        Point::new(0, 16),
+        text_style,
+        Baseline::Top,
+    )
+    .draw(&mut display)
+    .map_err(|e| anyhow::anyhow!("Txt2 error: {:?}", e))?;
 
-    display.flush().unwrap();
+    display
+        .flush()
+        .map_err(|e| anyhow::anyhow!("Flush error: {:?}", e))?;
+
+    Ok(())
+}
+
+fn test_wifi() -> Result<String> {
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    let sys_look_stack = Arc::new(EspSysLoopStack::new()?);
+    let nvs = Arc::new(EspDefaultNvs::new()?);
+
+    let mut wifi = EspWifi::new(netif_stack, sys_look_stack, nvs)?;
+
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: "iot-test".into(),
+        password: "test1234".into(),
+        ..Default::default()
+    }))?;
+
+    wifi.wait_status_with_timeout(Duration::from_secs(30), |s| !s.is_transitional())
+        .map_err(|e| anyhow::anyhow!("Wait timeout: {:?}", e))?;
+
+    let status = wifi.get_status();
+
+    println!("Status: {:?}", status);
+
+    if let ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(
+        client_settings,
+    ))) = status.0
+    {
+        Ok(format!("{:?}", client_settings.ip))
+    } else {
+        Err(anyhow::anyhow!("Failed to connect in time."))
+    }
+}
+
+fn main() {
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    esp_idf_sys::link_patches();
+
+    println!("Hello, Rust from an ESP32!");
+
+    //  configure peripherals
+    let peripherals = Peripherals::take().unwrap();
+    let mut led = peripherals.pins.gpio17.into_output().unwrap();
+    let mut led_onboard = peripherals.pins.gpio18.into_output().unwrap();
+
+    let wifi = test_wifi();
+    let ip = match wifi {
+        Err(e) => {
+            println!("Wifi error: {:?}", e);
+            format!("ERR: {:?}", e)
+        }
+        Ok(s) => s,
+    };
+
+    if let Err(e) = display_test(
+        peripherals.i2c0,
+        peripherals.pins.gpio4,
+        peripherals.pins.gpio5,
+        &ip,
+    ) {
+        println!("Display error: {:?}", e)
+    } else {
+        println!("Display ok");
+    }
 
     // heart-beat sequence
     for i in 0..20 {
@@ -69,6 +140,4 @@ fn main() {
 
         std::thread::sleep(Duration::from_millis(500));
     }
-
-    loop {}
 }
